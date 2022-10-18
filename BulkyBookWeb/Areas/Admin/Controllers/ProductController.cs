626036170aa112863c9e57a31953789e672c85c7
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-10-18 21:13:53 +0700

    fix controllerr

---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,83 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objCategoryList = _unitOfWork.Product.GetAll();
+            return View(objCategoryList);
+        }
+        //EDIT
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+            if (id == null || id == 0)
+            {
+                //create product
+                return View(product);
+            }
+            else
+            {
+                //update product
+            }
+            return View(product);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Edit object success!!!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //var CategoryFromDb = _db.Categories.Find(id);
+            //var CategoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            var CategoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            if (CategoryFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDbFirst);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Delete object success!!!";
+            return RedirectToAction("Index");
+        }
+    }
+}
