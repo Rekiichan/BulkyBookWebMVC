84a119e858699d8be2b76d49d0d63f2fad11a739
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-10-23 20:33:14 +0700

    asd

--- BulkyBookWeb/wwwroot/js/company.js
+++ BulkyBookWeb/wwwroot/js/company.js
@@ -10,23 +10,22 @@ function loadDataTable() {
             "url": "/Admin/Company/GetAll"
         },
         "columns": [
-            { "data": "name", "width": "45%" },
+            { "data": "name", "width": "15%" },
             { "data": "streetAddress", "width": "15%" },
-            { "data": "city", "width": "10%" },
-            { "data": "state", "width": "10%" },
-            { "data": "phoneNumber", "width": "10%" },
+            { "data": "city", "width": "15%" },
+            { "data": "state", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
             {
                 "data": "id",
-                "width": "10%",
                 "render": function (data) {
                     return `
-                         <div class="w-75 btn-group" role="group">
-                             <a href="/Admin/Company/Upsert?id=${data}"
-                                class="nav-link text-black" class="btn btn-primary" mx-2><i class="bi bi-pencil-square"></i>  Edit</a>
+                        <div class="w-75 btn-group" role="group">
+                            <a href="/Admin/Company/Upsert?id=${data}"
+                                class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
                             <a onClick=Delete('/Admin/Company/Delete/${data}')
-                                class="nav-link text-black" class="btn btn-primary" mx-2><i class="bi bi-pencil-square"></i>  Delete</a>
-                         </div>
-                            `
+                                class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+					    </div>
+                        `
                 }
             }
         ]
@@ -45,19 +44,19 @@ function Delete(url) {
             confirmButtonText: `Delete`
         }).then((result) => {
             if (result.isConfirmed)
-            $.ajax(
-                {
-                    url: url,
-                    type: 'DELETE',
-                    success: function (data) {
-                        if (data.success) {
-                            dataTable.ajax.reload();
-                            toastr.success(data.message);
-                        } else {
-                            toastr.error(data.message);
+                $.ajax(
+                    {
+                        url: url,
+                        type: 'DELETE',
+                        success: function (data) {
+                            if (data.success) {
+                                dataTable.ajax.reload();
+                                toastr.success(data.message);
+                            } else {
+                                toastr.error(data.message);
+                            }
                         }
-                    }
-                })
+                    })
         })
 }
 
